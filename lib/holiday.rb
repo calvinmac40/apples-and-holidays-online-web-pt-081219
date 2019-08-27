c6539b5034bab46f01a154a2e8ca2681d6f890c4
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 22:37:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do | season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday,supply|
-    puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{supply.join(", ")}"
+    puts "  #{holiday.split("_").map {|i| i.capitalize}.join(" ")}: #{supply.join(", ")}"
   end
 end
 end
